be4b4567baf3310edff7953533577bc34f611f09
Mate Papp <matepapp@icloud.com>
2016-07-07 14:31:51 +0200

    Initial Commit

---
+++ ReadeRSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadeRSS
+//
+//  Created by Mate Papp on 07/07/16.
+//  Copyright © 2016 Mate Papp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
