abf748e1b6b994070b1467b9fb0ead5e064c5a98
Mate Papp <matepapp@icloud.com>
2016-07-19 13:46:01 +0200

    Inserting almost works

--- ReadeRSS/MainTableViewController.swift
+++ ReadeRSS/MainTableViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    // MARK: Variables
+    
     var menuItems = ["All", "Unread", "Saved"]
     var topics = Category.allValues
     var feeds = [Feed]()
     
     var urls = [NSURL(string: "http://www.theverge.com/apple/rss/index.xml"), NSURL(string: "http://www.economist.com/rss/"), NSURL(string: "http://feeds.feedburner.com/techcrunch"), NSURL(string: "http://lifehacker.com/index.xml"), NSURL(string: "http://imagazin.hu/feed/"), NSURL(string: "http://index.hu/24ora/rss/"), NSURL(string: "https://github.com/matepapp.atom")]
     
-    var selectedIndex: NSIndexPath?
-    var selectedFeeds: [Feed]?
-
+    var selectedIndex: NSIndexPath? = nil
+    var selectedFeeds: [Feed]? = nil
+    
+    // MARK: Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,15 +30,10 @@ class MainTableViewController: UITableViewController {
         
         initializeFeeds()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func initializeFeeds() {
         // Setup loading screen.
-    
+        
         for url in urls {
             // TODO: Error handling
             XMLParser().parse(url!, handler: { (feedOptional: Feed?) in
@@ -52,31 +51,43 @@ class MainTableViewController: UITableViewController {
             })
         }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 2
     }
-
+    
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         switch section {
         case 0:
             return menuItems.count
             
         case 1:
-            return topics.count
+            return topics.count + (selectedFeeds?.count ?? 0)
         default:
             return 0
         }
     }
-
+    
+    override func tableView(tableView: UITableView,
+                            titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Menu"
+            
+        case 1:
+            return "Topics"
+        default:
+            return nil
+        }
+    }
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         print("tableview")
-    
+        
         // Reuse the cells with the identifier mainCell
         let cell = tableView.dequeueReusableCellWithIdentifier("mainCell", forIndexPath: indexPath) as! MainTableViewCell
         
@@ -85,77 +96,81 @@ class MainTableViewController: UITableViewController {
         case 0:
             switch indexPath.row {
             case 0:
-                let icon = UIImage(named: "all")!
-                let random = Int(arc4random_uniform(120) + 1)
-                cell.configureCell(icon, title: menuItems[indexPath.row], number: random)
+                cell.configureCell(UIImage(named: "all")!, title: menuItems[indexPath.row], number: Int(arc4random_uniform(120) + 1))
                 
             case 1:
-                let icon = UIImage(named: "unread")!
-                let random = Int(arc4random_uniform(120) + 1)
-                cell.configureCell(icon, title: menuItems[indexPath.row], number: random)
+                cell.configureCell(UIImage(named: "unread")!, title: menuItems[indexPath.row], number: Int(arc4random_uniform(120) + 1))
                 
             case 2: fallthrough
             default:
-                let icon = UIImage(named: "saved")!
-                let random = Int(arc4random_uniform(120) + 1)
-                cell.configureCell(icon, title: menuItems[indexPath.row], number: random)
+                cell.configureCell(UIImage(named: "saved")!, title: menuItems[indexPath.row], number: Int(arc4random_uniform(120) + 1))
             }
             
         // Initialize the second section's topics
         case 1:
-            let icon = UIImage(named: "down")
-            let random = Int(arc4random_uniform(120) + 1)
-            cell.configureCell(icon, title: "\(topics[indexPath.row])", number: random)
-            cell.accessoryType = UITableViewCellAccessoryType.None
+            if selectedIndex != nil {
+                // My most disgusting if condition ever
+                if calculateSelectedIndexPaths(indexPath).contains(NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)) {
+                    cell.configureCell(UIImage(named: "placeholder_icon"), title: selectedFeeds![selectedIndex!.row].name!, number: Int(arc4random_uniform(120) + 1))
+                } else {
+                    cell.configureCell(UIImage(named: "down"), title: "\(topics[indexPath.row])", number: Int(arc4random_uniform(120) + 1))
+                    cell.accessoryType = UITableViewCellAccessoryType.None
+                }
+            } else {
+                cell.configureCell(UIImage(named: "down"), title: "\(topics[indexPath.row])", number: Int(arc4random_uniform(120) + 1))
+                cell.accessoryType = UITableViewCellAccessoryType.None
+            }
             
         default:
             cell.configureCell(nil, title: "Default", number: 0)
         }
         return cell
     }
-    
-    override func tableView(tableView: UITableView,
-                   titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0:
-            return "Menu"
-        
-        case 1:
-            return "Topics"
-        default:
-            return nil
-        }
-    }
+
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         // If there is no row selected, select one
         if selectedIndex == nil {
-            // Insert as many rows as we need
-            insertRows(indexPath)
+            if indexPath.section == 1 {
+                // Insert as many rows as we need
+                insertRows(indexPath)
+            }
+            else {
+                self.performSegueWithIdentifier("ListTableView", sender: indexPath)
+            }
         }
-        
+            
         // If there is a row selected and we want to select that one again
         else if selectedIndex == indexPath {
             // Delete the previously inserted rows to collapse the topic
             deleteRows()
         }
-        
-        // There is another row selected 
-        else {
-            // Delete the previously inserted rows to collapse the other topic
-            deleteRows()
             
-            // Insert new rows to the freshly selected topic
-            insertRows(indexPath)
+        // There is another row selected
+        else {
+            if calculateSelectedIndexPaths(indexPath).contains(NSIndexPath(forRow: indexPath.row + 1, inSection: indexPath.section)) || indexPath.section == 0 {
+                self.performSegueWithIdentifier("ListTableView", sender: indexPath)
+            }
+            else {
+                // Delete the previously inserted rows to collapse the other topic
+                deleteRows()
+                
+                // Insert new rows to the freshly selected topic
+                insertRows(indexPath)
+            }
+
         }
         
-        
     }
-
+    
     // An insertion function which insert as many rows as we need from the index
     func insertRows(index: NSIndexPath) {
         // The selected index will be the indexPath
         selectedIndex = index
+        
+        guard let selectedIndex = selectedIndex else {
+            return
+        }
         
         // Calculate the selected feeds depending on the selected index
         selectedFeeds = selectFeedsFromCategory(topics[selectedIndex.row]) ?? [Feed]()
@@ -168,14 +183,19 @@ class MainTableViewController: UITableViewController {
     
     // A delete function which is delete the previously inserted rows
     func deleteRows() {
+        guard let selectedIndex = selectedIndex else {
+            return
+        }
         // Delete the rows from the previously selected topic
+        
         tableView.beginUpdates()
         tableView.deleteRowsAtIndexPaths(calculateSelectedIndexPaths(selectedIndex), withRowAnimation: .Fade)
         tableView.endUpdates()
         
+        
         // Restore the selected variables because nothing is selected
-        selectedIndex = nil
-        selectedFeeds = nil
+        self.selectedIndex = nil
+        self.selectedFeeds = nil
     }
     
     func selectFeedsFromCategory(cat: Category) -> [Feed]? {
@@ -184,39 +204,37 @@ class MainTableViewController: UITableViewController {
         })
     }
     
-    func calculateSelectedIndexPaths(index: NSIndexPath) -> [NSIndexpath]? {
+    func calculateSelectedIndexPaths(index: NSIndexPath) -> [NSIndexPath] {
         var indexes = [NSIndexPath]()
-        
-        for num in 0 ..< selectedFeeds?.count {
-            indexes.append(NSIndexPath(forRow: (indexPath.row + num), inSection: 1))
-        }
 
+        for num in 1 ... (selectedFeeds?.count ?? 0) {
+            indexes.append(NSIndexPath(forRow: (index.row + num), inSection: 1))
+        }
+ 
         return indexes
     }
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-        
         if segue.identifier == "ListTableView" {
             let destinationViewController = segue.destinationViewController as! ListTableViewController
+            
             // Get the index that generated this segue.
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 var selectedItem: Feed?
                 
-//                switch indexPath.section {
-//                case 0:
-//                    selectedItem = menuItems[indexPath.row]
-//                    
-//                case 1:
-//                    selectedItem = topics[indexPath.row]
-//                    
-//                default:
-//                    selectedItem = nil
-//                }
+                //                switch indexPath.section {
+                //                case 0:
+                //                    selectedItem = menuItems[indexPath.row]
+                //
+                //                case 1:
+                //                    selectedItem = topics[indexPath.row]
+                //
+                //                default:
+                //                    selectedItem = nil
+                //                }
                 
                 selectedItem = feeds[indexPath.row % urls.count]
                 
